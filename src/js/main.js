6128571878c4b7f27d728bfa9a64294b2cd4aa4d
goshatechnique <goshatechnique@gmail.com>
2026-02-13 16:27:22 +0300

    added modal, reworked quiz question render function

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,15 @@ console.log("Сайт запущен!");
 
 import { initCarousel } from "./components/carousel";
 
+import { initModal } from "./components/modal";
+
+import { renderQuestion } from "./components/renderQuestions";
+
 document.addEventListener("DOMContentLoaded", () => {
+	const tagsContainer = document.querySelector(".quiz__test-tags");
+	if (tagsContainer) {
+		renderQuestion(1);
+	}
 	initCarousel();
+	initModal("quizModal", ".open-quiz-modal");
 });
